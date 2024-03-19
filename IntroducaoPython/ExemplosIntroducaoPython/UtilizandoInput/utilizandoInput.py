a23af28ba722ad1012b27a3687ad11f24cfce154
guilhermecaetano1010 <guilherme.caetano1010@gmail.com>
2024-03-18 22:12:08 -0300

    Adicionando exemplo utilizando inputs

---
+++ IntroducaoPython/ExemplosIntroducaoPython/UtilizandoInput/utilizandoInput.py
@@ -0,0 +1,14 @@
+# Comentário de uma única linha
+'''
+Comentário de múltiplas linhas
+'''
+
+name = input("Digite o nome do jogo: \n")
+yearLaunch = int(input("Digite o ano de lançamento do jogo: \n"))
+gamePrice = float(input("Digite o preço do jogo: \n"))
+planIncluded = input("Está incluso no serviço mensal?\n")
+
+print(name)
+print(yearLaunch)
+print(gamePrice)
+print(planIncluded)
